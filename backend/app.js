5fb74a8d5d1131b5f9653de3f091fa3602377297
Vrinda Vyas <vrindavyas10@gmail.com>
2022-06-28 18:38:12 +0530

    created first api call and parsing json data

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,26 @@ const app = express()
 
 require('dotenv/config')
 const api = process.env.API_URL
+// middleware
+app.use(express.json())
 
-app.get(api+'/products',(req, res) => {
-    res.send('hello API')
+// app.get(api+'/products',(req, res) => {
+//     res.send('hello API')
+// })
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: "Dell laptop",
+        image: 'some_url',
+    }
+    res.send(product)
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct)
+    res.send(newProduct)
 })
 
 app.listen(3000, () => {

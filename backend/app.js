35833dc90d6a6e4d4625ef25083e8ddaaa91947b
Vrinda Vyas <vrindavyas10@gmail.com>
2022-06-29 12:54:26 +0530

    post data using api

--- backend/app.js
+++ backend/app.js
@@ -12,19 +12,38 @@ const DB = process.env.DB_URL
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "Dell laptop",
-        image: 'some_url',
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock:{
+        type: Number,
+        required: true
     }
-    res.send(product)
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+app.get(`${api}/products`, (req, res) => {
+    const proudctList = Product.find()
+    res.send(proudctList)
 })
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
-    console.log(newProduct)
-    res.send(newProduct)
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) =>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+    
 })
 
 mongoose.connect(DB)

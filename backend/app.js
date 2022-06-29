f29b6054bcda0b7bc1e7b0790cdaa3c8f675ec89
Vrinda Vyas <vrindavyas10@gmail.com>
2022-06-29 15:30:46 +0530

    created backend api routes & schema

--- backend/app.js
+++ backend/app.js
@@ -1,59 +1,48 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// require('dotenv/config');
+require('dotenv').config()
 
-require('dotenv/config')
-const api = process.env.API_URL
-const DB = process.env.DB_URL
+app.use(cors());
+app.options('*', cors())
 
-// middleware
-app.use(bodyParser.json())
-app.use(morgan('tiny'))
+//middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock:{
-        type: Number,
-        required: true
-    }
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-db'
+})
+.then(()=>{
+    console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+    console.log(err);
 })
 
-const Product = mongoose.model('Product', productSchema)
+//Server
+app.listen(3000, ()=>{
 
-app.get(`${api}/products`, (req, res) => {
-    const proudctList = Product.find()
-    res.send(proudctList)
+    console.log('server is running http://localhost:3000');
 })
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) =>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-    
-})
-
-mongoose.connect(DB)
-.then(() => {
-    console.log('Success !!!')
-}).catch((err) => {
-    console.log(err)
-})
-
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000")
-})
-

fa7ab11d37f744b670dad1b5b26bd45d0521be07
Vrinda Vyas <vrindavyas10@gmail.com>
2022-07-30 15:06:03 +0530

    Created admin panel, eshop and added routing

---
+++ frontend/eshop/apps/eshop/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+     path: 'products',
+    component: ProductListComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomePageComponent,
+    ProductListComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

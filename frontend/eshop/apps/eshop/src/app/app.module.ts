d08357a4fef6db20cce9fd9f0250c5723b147c85
Vrinda Vyas <vrindavyas10@gmail.com>
2022-10-02 16:31:09 +0530

    Eslint Rules Update with Template Removal

--- frontend/eshop/apps/eshop/src/app/app.module.ts
+++ frontend/eshop/apps/eshop/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -23,7 +22,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
     HomePageComponent,
     ProductListComponent,
     HeaderComponent,
